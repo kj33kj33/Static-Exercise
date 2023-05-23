1c6089e0ba8e7e91eea349fcb3bdd3cb65284e9d
James <jamesmcdaniel33@hotmail.com>
2023-05-23 16:36:41 -0500

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double tempFahrenheit = 98.5;
+            Console.WriteLine("Please enter a temperature to convert to Celsius:");
+
+            while (!double.TryParse(Console.ReadLine(), out tempFahrenheit))
+            {
+                Console.WriteLine("Invalid input\nPlease input a temperature to convert to Celsius:");
+            }
+
+            double tempCelsius = TempConverter.FahrenheitToCelsius(tempFahrenheit);
+            Console.WriteLine($"\n{tempFahrenheit} degrees Fahrenheit equals {tempCelsius} degrees Celsius.\n");
+
+            tempFahrenheit = TempConverter.CelsiusToFahrenheit(tempCelsius);
+            Console.WriteLine($"{tempCelsius} degrees Celsius equals {tempFahrenheit} degrees Fahrenheit.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double temperature)
+		{
+			return (temperature - 32) / 1.8;
+		}
+
+		public static double CelsiusToFahrenheit(double temperature)
+		{
+			return (1.8 * temperature) + 32;
+		}
+	}
+}
+
